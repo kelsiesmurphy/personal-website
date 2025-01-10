93b07861865429ea954054993015b1b0622e486c
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2025-01-10 19:56:30 +0000

    feat: add Badge component, update package configurations, and adjust import paths

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kelsiesmurphy.com",
   integrations: [tailwind({
     applyBaseStyles: false,
-  })]
+  }), react()]
 });
