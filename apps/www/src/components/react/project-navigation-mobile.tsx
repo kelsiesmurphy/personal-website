cfd00ffc7140ee26c596c6d164efdfe6df8b54eb
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2025-01-20 15:22:59 +0000

    feat: add mobile project navigation component; update project navigation to support current project context

---
+++ apps/www/src/components/react/project-navigation-mobile.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@repo/ui/components/ui/select";
+
+export type Project = {
+  name: string;
+  slug: string;
+  description: string;
+  repo: string;
+  url: string;
+};
+
+export default function ProjectNavMobile({
+  projects,
+  currentProjectName,
+}: {
+  projects: Project[];
+  currentProjectName: string | undefined;
+}) {
+  return (
+    <div className="flex md:hidden">
+      <Select>
+        <SelectTrigger className="px-0 border-none font-nord font-bold text-lg">
+          <SelectValue placeholder={`Project ${currentProjectName}`} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {projects.map((project) => {
+              return (
+                <SelectItem value={project.slug}>
+                  <a className="font-normal" href={`/projects/${project.slug}`}>
+                    Project {project.name}
+                  </a>
+                </SelectItem>
+              );
+            })}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
--- apps/www/src/components/react/project-navigation.tsx
+++ apps/www/src/components/react/project-navigation.tsx
@@ -1,28 +1,41 @@
 import { Button } from "@repo/ui/components/ui/button";
 import projects from "@repo/projects-config/projects.json";
+import ProjectNavMobile from "./project-navigation-mobile";
 
-export default function ProjectNavigation() {
+export default function ProjectNavigation({
+  currentProjectName,
+}: {
+  currentProjectName: string | undefined;
+}) {
   return (
-    <nav className="container py-4 flex items-center justify-between gap-6">
-      <div className="flex gap-8 items-center">
-        <h1 className="font-nord font-bold text-2xl">Projects</h1>
-        <ul className="flex gap-8 flex-wrap">
-          {projects.map((project) => (
-            <li key={project.slug}>
-              <Button variant="link" className="px-2" asChild>
-                <a className="font-normal" href={`/projects/${project.slug}`}>
-                  {project.name.toLocaleLowerCase()}
-                </a>
-              </Button>
-            </li>
-          ))}
-        </ul>
+    <nav className="py-3 border-b border-border">
+      <div className="container flex items-center justify-between gap-6">
+        <div className="flex gap-8 items-center">
+          <h1 className="hidden md:block font-nord font-bold text-lg">
+            Project {currentProjectName}
+          </h1>
+          <ProjectNavMobile
+            projects={projects}
+            currentProjectName={currentProjectName}
+          />
+          <ul className="hidden md:flex gap-8 flex-wrap">
+            {projects.map((project) => (
+              <li key={project.slug}>
+                <Button variant="link" className="px-2" asChild>
+                  <a className="font-normal" href={`/projects/${project.slug}`}>
+                    {project.name.toLocaleLowerCase()}
+                  </a>
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Button variant="link" className="px-2" asChild>
+          <a className="font-normal" href="/">
+            home
+          </a>
+        </Button>
       </div>
-      <Button variant="link" className="px-2" asChild>
-        <a className="font-normal" href="/">
-          home
-        </a>
-      </Button>
     </nav>
   );
 }
